5c49643297ad7274a19a181c3ed6df293be9f48c
gizilaalex-del <gizila.alex@gmail.com>
2026-05-30 12:41:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,3 +30,23 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+    
+import os
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
+
+bot.polling()
+
+
